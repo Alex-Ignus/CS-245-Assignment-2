fe946a36725a643c4ca4c90383ead565ce31210d
Alex-wojcik <alex.wojcik@coverhound.com>
2019-12-07 13:59:59 -0800

    update

--- src/CSV.java
+++ src/CSV.java
@@ -1,19 +1,25 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.org.json.simple.JSONObject;
 import java.org.json.simple.parser.JSONParser;
 import java.org.json.simple.parser.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CSV {
     private JsonHealper jsonText;
 
   public List<List<Object>> csvReader(){
       List<List<Object>> records = new ArrayList<>();
-      try (BufferedReader br = new BufferedReader(new FileReader("/Users/awojcik/Documents/GitHub/CS 245 - Assignment 2/CS-245-Assignment-2/src/tmdb_5000_credits.csv"))) {
+          try (BufferedReader br = Files.newBufferedReader(Paths.get("/Users/awojcik/Documents/GitHub/CS 245 - Assignment 2/CS-245-Assignment-2/src/tmdb_5000_credits.csv"),
+                  StandardCharsets.UTF_8)) {
           Object line;
           while ((line = br.readLine()) != null) {
               Object[] values = ((String) line).split(",");
@@ -38,10 +44,14 @@ public class CSV {
   }
 
   public void cleanList(List<List<Object>> records) {
+      String s = "";
+      String regex ="\\\"([^\\\"]*)\\\"\\W*";
       for (List<Object> internal: records) {
           for(int i = 0; i < internal.size(); i++){
               if(internal.get(i).toString().contains("name")){
-                  System.out.println(internal.get(i));
+                  s = internal.get(i).toString().replaceAll(regex," ");
+                  System.out.println( s + " | my index: " + i);
+                  
               }
           }
       }
